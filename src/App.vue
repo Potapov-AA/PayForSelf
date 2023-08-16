56c64a90fd31485095736c439c983ad0b8b441f6
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-16 15:55:14 +0300

    Init project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import MyHeader from './components/MyHeader.vue';
+
+export default {
+  components: {
+    'my-header': MyHeader,
+  }
+}
+</script>
+
+<template>
+  <v-app>
+    <my-header></my-header>
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<style scoped></style>
