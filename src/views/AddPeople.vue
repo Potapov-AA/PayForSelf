fbd585473d4dac1a5c1e1dec70deb9e46e85ee2a
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-16 20:59:10 +0300

    Сделана декомпозиция элементов

--- src/views/AddPeople.vue
+++ src/views/AddPeople.vue
@@ -1,30 +1,28 @@
 <template>
   <v-container class="d-flex flex-column align-center">
-    <v-btn @click="store.addPerson()">
-        добавить человека
-    </v-btn>
-    
-    <div v-if="store.people.length > 0">
-        <div v-for="(person, index) in store.people" :key="person.id">
-            {{ person.id }} TEST
-            {{ index }}
-            <input type="text" v-model="person.name" id="" placeholder="Введите имя">
-            <v-btn :key="person.id" @click="store.deletePerson(index)">Удалить</v-btn>
-        </div>
-    </div>
+    <v-btn @click="storePeople.addPerson()"> добавить человека </v-btn>
+        <my-person-list></my-person-list>
+    <div v-if="storePeople.people.length > 0"></div>
     <div v-else>
-        <h1 class="text-uppercase">за столом как-то пусто</h1>
+      <h1 class="text-uppercase">за столом как-то пусто</h1>
     </div>
   </v-container>
 </template>
 
-<script setup>
+<script>
 import { usePeopleStore } from '@/stores/people'
+import MyPersonList from '@/components/MyPersonList.vue'
 
-const store = usePeopleStore();
-
+export default {
+  data() {
+    return {
+      storePeople: usePeopleStore()
+    }
+  },
+  components: {
+    'my-person-list': MyPersonList,
+  }
+}
 </script>
 
-<style>
-
-</style>
+<style></style>
