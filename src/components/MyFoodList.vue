bdd05d4558d3129894f91f3a358b8dfed1dff3ab
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-16 20:57:09 +0300

    Компонент списка продуктов

---
+++ src/components/MyFoodList.vue
@@ -0,0 +1,35 @@
+<template>
+  <keep-alive>
+    <div v-for="(food, index) in storeFoods.foods" :key="food.id">
+      {{ food.id }} TEST {{ index }}
+      <input type="text" v-model="food.name" id="" placeholder="Введите название продукта" />
+      <input type="number" v-model="food.cost" id="" placeholder="Введите стоимость" />
+      <v-btn :key="food.id" @click="storeFoods.deleteFood(index)">Удалить</v-btn>
+
+      <div v-for="person in storePeople.people" :key="person.id">
+        <my-eater :person="person" :index="index"></my-eater>
+      </div>
+    </div>
+  </keep-alive>
+</template>
+
+<script>
+import { usePeopleStore } from '@/stores/people'
+import { useFoodsStore } from '@/stores/foods'
+
+import MyEater from './MyEater.vue'
+
+export default {
+  data() {
+    return {
+      storePeople: usePeopleStore(),
+      storeFoods: useFoodsStore()
+    }
+  },
+  components: {
+    'my-eater': MyEater
+  }
+}
+</script>
+
+<style></style>
