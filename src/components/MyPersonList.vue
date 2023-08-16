646a2c3eaaf6381e4c223d3c5d88ce4ca85859e6
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-16 20:57:20 +0300

    Компонент списка персон

---
+++ src/components/MyPersonList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-for="(person, index) in storePeople.people" :key="person.id">
+    {{ person.id }} TEST {{ index }}
+    <input type="text" v-model="person.name" id="" placeholder="Введите имя" />
+    <v-btn :key="person.id" @click="storePeople.deletePerson(index)">Удалить</v-btn>
+  </div>
+</template>
+
+<script>
+import { usePeopleStore } from '@/stores/people'
+
+export default {
+  data() {
+    return {
+      storePeople: usePeopleStore()
+    }
+  }
+}
+</script>
+
+<style></style>
