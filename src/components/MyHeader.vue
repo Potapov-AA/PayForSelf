a89e863d2b1ebe56336a6a189d589105be4f87c3
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-16 15:55:43 +0300

    Сделан компонент Header

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,49 @@
+<template>
+  <header class="elevation-10 pa-5">
+    <v-row class="mb-2">
+      <v-spacer></v-spacer>
+      <v-col align="center"><p class="text-h4"><strong>PayForSelf</strong></p></v-col>
+      <v-spacer></v-spacer>
+    </v-row>
+    <v-row>
+      <v-spacer></v-spacer>
+      <v-col align="center">
+        <my-header-item
+          :iconName="'mdi-numeric-1'"
+          :tooltipText="'Добавь тех, кто ел'"
+          :size="40"
+          :link="''"
+        ></my-header-item>
+      </v-col>
+      <v-col align="center">
+        <my-header-item
+          :iconName="'mdi-numeric-2'"
+          :tooltipText="'Добавь то, что ели'"
+          :size="40"
+          :link="'foods'"
+        ></my-header-item>
+      </v-col>
+      <v-col align="center">
+        <my-header-item
+          :iconName="'mdi-numeric-3'"
+          :tooltipText="'Посмотри, кто сколько наел'"
+          :size="40"
+          :link="'results'"
+        ></my-header-item>
+      </v-col>
+      <v-spacer></v-spacer>
+    </v-row>
+  </header>
+</template>
+
+<script>
+import MyHeaderItem from './MyHeaderItem.vue'
+
+export default {
+  components: {
+    'my-header-item': MyHeaderItem
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/MyHeaderItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-tooltip location="bottom" :text="tooltipText">
+    <template v-slot:activator="{ props }">
+      <v-btn variant="outlined" icon v-bind="props" value="nearby" @click="$router.push('/' + link)"> 
+        <v-icon :size="size">{{ iconName }}</v-icon>
+      </v-btn>
+    </template>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: String,
+    tooltipText: String,
+    size: Number,
+    link: String
+  }
+}
+</script>
+
+<style></style>
