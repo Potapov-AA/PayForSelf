b80fd72245532103e74faed4c6a54f76ec62497f
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-16 17:09:44 +0300

    Прописана основная логика добавления новых людей и сделана связь с хранилищем people

--- src/views/AddPeople.vue
+++ src/views/AddPeople.vue
@@ -1,17 +1,19 @@
 <template>
   <v-container class="d-flex flex-column align-center">
-    <v-btn @click="store.increment">
+    <v-btn @click="store.addPerson()">
         добавить человека
     </v-btn>
-    <div v-if="false">
-        <div v-for="k in 5" :key="k">
-            {{ k }}
+    
+    <div v-if="store.people.length > 0">
+        <div v-for="(person, index) in store.people" :key="person.id">
+            {{ person.id }} TEST
+            {{ index }}
+            <input type="text" v-model="person.name" id="" placeholder="Введите имя">
+            <v-btn :key="person.id" @click="store.deletePerson(index)">Удалить</v-btn>
         </div>
     </div>
     <div v-else>
         <h1 class="text-uppercase">за столом как-то пусто</h1>
-        {{ store.count }}
-        {{ store.doubleCount }}
     </div>
   </v-container>
 </template>
