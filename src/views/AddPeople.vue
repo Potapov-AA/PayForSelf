4e80e819b59f27b6da9f0ad6ad0926ca0b2af01b
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-23 13:00:08 +0300

    Добавил иконку добавления человека

--- src/views/AddPeople.vue
+++ src/views/AddPeople.vue
@@ -1,15 +1,17 @@
 <template>
-  <transition  name="btn-plus-animation" mode="out-in">
+  <transition name="btn-plus-animation" mode="out-in">
     <v-container
       v-if="storePeople.people.length == 0"
       class="d-flex flex-column align-center justify-center h-100"
     >
       <v-btn variant="text" icon size="150">
-        <v-icon @click="storePeople.addPerson()" size="150">mdi-plus-circle</v-icon> </v-btn
-      >\
+        <v-icon @click="storePeople.addPerson()" size="150">mdi-plus-circle</v-icon>
+      </v-btn>
     </v-container>
     <v-container v-else class="d-flex flex-column justify-center align-center">
-      <v-btn @click="storePeople.addPerson()" class="my-5"> добавить человека </v-btn>
+      <v-btn @click="storePeople.addPerson()" class="my-5">
+      <v-icon size="30">mdi-account-plus</v-icon>
+      </v-btn>
 
       <div v-if="storePeople.people.length > 0">
         <my-person-list></my-person-list>
@@ -38,11 +40,13 @@ export default {
 </script>
 
 <style>
-.btn-plus-animation-enter-active, .btn-plus-animation-leave-active {
-  transition: opacity .5s ease
+.btn-plus-animation-enter-active,
+.btn-plus-animation-leave-active {
+  transition: opacity 0.5s ease;
 }
 
-.btn-plus-animation-enter-from, .btn-plus-animation-leave-to {
-  opacity: 0
+.btn-plus-animation-enter-from,
+.btn-plus-animation-leave-to {
+  opacity: 0;
 }
 </style>
