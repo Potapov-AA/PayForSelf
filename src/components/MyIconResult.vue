248a0a64a7e40987945603defce6484fa3203304
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-23 16:19:51 +0300

    Компонент иконки-вкладки

---
+++ src/components/MyIconResult.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-tooltip location="top" :text="tooltipText">
+    <template v-slot:activator="{ props }">
+      <v-btn size="50" variant="text" icon v-bind="props" @click="this.$emit('show')">
+        <v-icon size="35">{{ icon }}</v-icon>
+      </v-btn>
+    </template>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+    props: {
+        icon: String,
+        tooltipText: String,
+        eventName: String
+    }
+}
+</script>
+
+<style></style>
