368b0d82f6b80aa45365392c9adc94df83c08cb7
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-23 12:23:28 +0300

    Добавил анимацию переход от добавления первого человека к списку персон

--- src/views/AddPeople.vue
+++ src/views/AddPeople.vue
@@ -1,14 +1,24 @@
 <template>
-  <v-container class="d-flex flex-column justify-center align-center">
-    <v-btn @click="storePeople.addPerson()" class="my-5"> добавить человека </v-btn>
+  <transition  name="btn-plus-animation" mode="out-in">
+    <v-container
+      v-if="storePeople.people.length == 0"
+      class="d-flex flex-column align-center justify-center h-100"
+    >
+      <v-btn variant="text" icon size="150">
+        <v-icon @click="storePeople.addPerson()" size="150">mdi-plus-circle</v-icon> </v-btn
+      >\
+    </v-container>
+    <v-container v-else class="d-flex flex-column justify-center align-center">
+      <v-btn @click="storePeople.addPerson()" class="my-5"> добавить человека </v-btn>
 
-    <div v-if="storePeople.people.length > 0">
-      <my-person-list></my-person-list>
-    </div>
-    <div v-else>
-      <h1 class="text-uppercase">за столом как-то пусто</h1>
-    </div>
-  </v-container>
+      <div v-if="storePeople.people.length > 0">
+        <my-person-list></my-person-list>
+      </div>
+      <div v-else>
+        <h1 class="text-uppercase">за столом как-то пусто</h1>
+      </div>
+    </v-container>
+  </transition>
 </template>
 
 <script>
@@ -27,4 +37,12 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.btn-plus-animation-enter-active, .btn-plus-animation-leave-active {
+  transition: opacity .5s ease
+}
+
+.btn-plus-animation-enter-from, .btn-plus-animation-leave-to {
+  opacity: 0
+}
+</style>
