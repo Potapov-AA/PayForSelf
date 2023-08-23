70bdf15a040d6072132dff4e3fb74e4c6a09e670
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-23 20:25:27 +0300

    Добавил стили

--- src/components/MyIconResult.vue
+++ src/components/MyIconResult.vue
@@ -1,7 +1,7 @@
 <template>
   <v-tooltip location="top" :text="tooltipText">
     <template v-slot:activator="{ props }">
-      <v-btn size="50" variant="text" icon v-bind="props" @click="this.$emit('show')">
+      <v-btn size="50" variant="text" icon v-bind="props" @click="this.$emit('show')" :class="{ active: isActive }">
         <v-icon size="35">{{ icon }}</v-icon>
       </v-btn>
     </template>
@@ -13,9 +13,15 @@ export default {
     props: {
         icon: String,
         tooltipText: String,
-        eventName: String
+        eventName: String,
+        isActive: Boolean
     }
 }
 </script>
 
-<style></style>
+<style scoped>
+.active {
+    border: 1px solid black !important;
+    color:  black;
+}
+</style>
