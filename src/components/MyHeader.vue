aaf83a3bcad017b6d3fd6f09b9b3b8cdab4ff41d
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-23 12:22:00 +0300

    Добавил декоративные стрелки

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,37 +1,41 @@
 <template>
-  <header class="elevation-10 pa-5">
+  <header class="elevation-10 pa-4">
     <v-row class="mb-2">
       <v-spacer></v-spacer>
       <v-col align="center"><p class="text-h4"><strong>PayForSelf</strong></p></v-col>
       <v-spacer></v-spacer>
     </v-row>
-    <v-row>
-      <v-spacer></v-spacer>
-      <v-col align="center">
+    <v-row class="d-flex justify-center mb-1">
+      <div>
         <my-header-item
           :iconName="'mdi-numeric-1'"
           :tooltipText="'Добавь тех, кто ел'"
           :size="40"
           :link="''"
         ></my-header-item>
-      </v-col>
-      <v-col align="center">
+      </div>
+      <div>
+        <v-icon :size="45">mdi-arrow-right</v-icon>
+      </div>
+      <div>
         <my-header-item
           :iconName="'mdi-numeric-2'"
           :tooltipText="'Добавь то, что ели'"
           :size="40"
           :link="'foods'"
         ></my-header-item>
-      </v-col>
-      <v-col align="center">
+      </div>
+      <div>
+        <v-icon :size="45">mdi-arrow-right</v-icon>
+      </div>
+      <div>
         <my-header-item
           :iconName="'mdi-numeric-3'"
           :tooltipText="'Посмотри, кто сколько наел'"
           :size="40"
           :link="'results'"
         ></my-header-item>
-      </v-col>
-      <v-spacer></v-spacer>
+      </div>
     </v-row>
   </header>
 </template>
