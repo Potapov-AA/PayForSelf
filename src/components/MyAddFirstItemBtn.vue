aec12ddb3f515f7d9bb950c64736ed0509976f21
Jester <jestterr@mail.ru>
2023-08-29 09:59:13 +0300

    Создал компонент добавления первого элемента

---
+++ src/components/MyAddFirstItemBtn.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-btn variant="text" icon size="150">
+    <v-icon @click="addItemFunction" size="150">mdi-plus-circle</v-icon>
+  </v-btn>
+</template>
+
+<script>
+export default {
+    props: {
+        addItemFunction: Function
+    }
+}
+</script>
+
+<style></style>
