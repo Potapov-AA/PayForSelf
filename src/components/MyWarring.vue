9e1a1c2bf2c9631b14816f83d3e3be689527791c
Jester <jestterr@mail.ru>
2023-08-29 10:02:39 +0300

    Компонент сообщения-предупреждения о некорректности данных

---
+++ src/components/MyWarring.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="h-auto w-auto pa-5 rounded-lg warring">
+    <p align="center" class="text-h5"><strong>ПРЕДУПРЕЖДЕНИЕ</strong></p>
+    <p align="center" class="mt-5">{{ warringText }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    warringText: String
+  }
+}
+</script>
+
+<style>
+.warring {
+  border: black dashed 2px;
+}
+</style>
