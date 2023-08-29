aa15b068ee0fbee486394a5109d075a58d828a0c
Jester <jestterr@mail.ru>
2023-08-29 10:04:32 +0300

    Декомпозиция элемента добавления первой персоны

--- src/views/AddPeople.vue
+++ src/views/AddPeople.vue
@@ -4,28 +4,23 @@
       v-if="storePeople.people.length == 0"
       class="d-flex flex-column align-center justify-center h-100"
     >
-      <v-btn variant="text" icon size="150">
-        <v-icon @click="storePeople.addPerson()" size="150">mdi-plus-circle</v-icon>
-      </v-btn>
+      <my-add-first-item-btn :addItemFunction="storePeople.addPerson"></my-add-first-item-btn>
     </v-container>
     <v-container v-else class="d-flex flex-column justify-center align-center">
       <v-btn @click="storePeople.addPerson()" class="my-5">
-      <v-icon size="30">mdi-account-plus</v-icon>
+        <v-icon size="30">mdi-account-plus</v-icon>
       </v-btn>
 
-      <div v-if="storePeople.people.length > 0">
-        <my-person-list></my-person-list>
-      </div>
-      <div v-else>
-        <h1 class="text-uppercase">за столом как-то пусто</h1>
-      </div>
+      <my-person-list></my-person-list>
     </v-container>
   </transition>
 </template>
 
 <script>
 import { usePeopleStore } from '@/stores/people'
+
 import MyPersonList from '@/components/MyPersonList.vue'
+import MyAddFirstItemBtn from '../components/MyAddFirstItemBtn.vue'
 
 export default {
   data() {
@@ -34,19 +29,10 @@ export default {
     }
   },
   components: {
-    'my-person-list': MyPersonList
+    'my-person-list': MyPersonList,
+    'my-add-first-item-btn': MyAddFirstItemBtn
   }
 }
 </script>
 
-<style>
-.btn-plus-animation-enter-active,
-.btn-plus-animation-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.btn-plus-animation-enter-from,
-.btn-plus-animation-leave-to {
-  opacity: 0;
-}
-</style>
+<style></style>
