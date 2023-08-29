4a288acd581f0a2ada098c5feae0269cd486b63c
Jester <jestterr@mail.ru>
2023-08-29 09:58:06 +0300

    Привел к общей структуре

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
-<script>
-import MyHeader from '@/components/MyHeader.vue';
-
-export default {
-  components: {
-    'my-header': MyHeader,
-  }
-}
-</script>
-
 <template>
   <v-app>
     <my-header></my-header>
     <router-view></router-view>
   </v-app>
 </template>
+
+<script>
+import MyHeader from '@/components/MyHeader.vue'
+export default {
+  components: {
+    'my-header': MyHeader
+  }
+}
+</script>
 
 <style scoped></style>
