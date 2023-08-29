afebf4599dc3c236ddd1e200483b4650ccb54403
Jester <jestterr@mail.ru>
2023-08-29 10:04:58 +0300

    Добавлена валидация и вывод сообщения-предупреждения

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-container>
+  <v-container
+    v-if="checkFoodListFields()"
+    class="d-flex flex-column align-center justify-center h-100"
+  >
+    <my-warring :warringText="warringMessage"></my-warring>
+  </v-container>
+  <v-container v-else>
     <div class="d-flex justify-space-evenly my-5">
       <my-icon-result
         @show="showTableComponent"
@@ -36,24 +42,28 @@ import { usePeopleStore } from '@/stores/people'
 import { useFoodsStore } from '@/stores/foods'
 import MyIconResult from '@/components/MyIconResult.vue'
 import MyTableResult from '@/components/MyTableResult.vue'
-import MyStatistic from '../components/MyStatistic.vue'
+import MyStatistic from '@/components/MyStatistic.vue'
+import MyWarring from '@/components/MyWarring.vue'
 
-
+import MyMixinValidate from '@/components/MyMixinValidate.vue'
 
 export default {
+  mixins: [MyMixinValidate],
   data() {
     return {
       storePeople: usePeopleStore(),
       storeFoods: useFoodsStore(),
       showTable: true,
       showStatistic: false,
-      showDebtor: false
+      showDebtor: false,
+      warringMessage: ''
     }
   },
   components: {
     'my-icon-result': MyIconResult,
     'my-table-result': MyTableResult,
-    'my-statistic': MyStatistic
+    'my-statistic': MyStatistic,
+    'my-warring': MyWarring
   },
   methods: {
     showTableComponent() {
@@ -70,6 +80,39 @@ export default {
       this.showTable = false
       this.showStatistic = false
       this.showDebtor = true
+    },
+    checkFoodListFields() {
+      var show = false
+
+      if (this.showWarringMessage()) {
+        this.warringMessage = this.warringText
+        show = true
+      } else {
+        if (this.warringText == '') {
+          if (this.storeFoods.foods.length == 0) {
+            this.warringMessage =
+              'Для отображения результатов необходимо добавить блюда и заполнить все поля.'
+            show = true
+          } else {
+            this.storeFoods.foods.forEach((food) => {
+              if (food.name == '' || food.cost == '' || food.payerId == 'Кто платит?') {
+                this.warringMessage = 'Для отображения результатов необходимо заполнить все поля у каждого блюда.'
+                show = true
+              } else if (isNaN(Number(food.cost)) || food.cost <= 0) {
+                this.warringMessage =
+                  'Проверьте поле стоимости блюда, возможно где-то введено некорретное число.'
+                show = true
+              } else if (food.eaters.length == 0) {
+                this.warringMessage =
+                  'Проверьте, что у каждого блюда выбраны те, кто его ел.'
+                show = true
+              }
+            })
+          }
+        }
+      }
+
+      return show
     }
   }
 }
