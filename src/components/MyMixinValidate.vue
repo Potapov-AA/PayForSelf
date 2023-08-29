541aac7178b6d889c99c5eac115e710cc1906a1f
Jester <jestterr@mail.ru>
2023-08-29 10:02:10 +0300

    Перенес переиспользуемую функцию валидации в миксины

---
+++ src/components/MyMixinValidate.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  data() {
+    return {
+      warringText: ''
+    }
+  },
+
+  methods: {
+    showWarringMessage() {
+      var showWarning = false
+
+      if (this.storePeople.people.length == 0) {
+        showWarning = true
+        this.warringText = 'Прежде чем начать, пожалуйста добавьте людей.'
+      } else if (this.storePeople.people.length == 1) {
+        showWarning = true
+        this.warringText = 'Проверьте, что добавлено больше одного человека.'
+      } else {
+        this.storePeople.people.forEach((person) => {
+          if (person.name === '') {
+            showWarning = true
+            this.warringText = 'Проверьте, что всем добавленым людям записаны имена.'
+          }
+        })
+      }
+
+      return showWarning
+    }
+  }
+}
+</script>
+
+<style></style>
