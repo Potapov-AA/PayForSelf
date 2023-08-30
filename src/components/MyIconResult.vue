122b19984a9ab54f1d2856b749dd6766977fb9e5
Alexandr Potapov <San_sanih99@mail.ru>
2023-08-30 16:27:48 +0300

    Перенес стили в main.scss

--- src/components/MyIconResult.vue
+++ src/components/MyIconResult.vue
@@ -1,7 +1,7 @@
 <template>
   <v-tooltip location="top" :text="tooltipText">
     <template v-slot:activator="{ props }">
-      <v-btn size="50" variant="text" icon v-bind="props" @click="this.$emit('show')" :class="{ active: isActive }">
+      <v-btn size="50" variant="text" icon v-bind="props" @click="this.$emit('show')" :class="{ activeResultsPage: isActive }">
         <v-icon size="35">{{ icon }}</v-icon>
       </v-btn>
     </template>
@@ -19,9 +19,5 @@ export default {
 }
 </script>
 
-<style scoped>
-.active {
-    border: 1px solid black !important;
-    color:  black;
-}
+<style>
 </style>
