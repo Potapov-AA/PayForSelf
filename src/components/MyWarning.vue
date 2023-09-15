222e16678a288170b6f0ad2a4546cfce4f623a31
Alexandr Potapov <San_sanih99@mail.ru>
2023-09-15 20:20:02 +0300

    Убраны пустые секции; Исправлен нэйминг

--- src/components/MyWarning.vue
+++ src/components/MyWarning.vue
@@ -1,17 +1,14 @@
 <template>
   <div class="h-auto w-auto pa-5 rounded-lg warring-message-border">
     <p align="center" class="text-h5"><strong>ПРЕДУПРЕЖДЕНИЕ</strong></p>
-    <p align="center" class="mt-5">{{ warringText }}</p>
+    <p align="center" class="mt-5">{{ warningText }}</p>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    warringText: String
+    warningText: String
   }
 }
 </script>
-
-<style>
-</style>
