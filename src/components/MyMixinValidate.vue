a2498c81b256fa407afd76c6772c8e53469513c8
Alexandr Potapov <San_sanih99@mail.ru>
2023-09-29 18:42:54 +0300

    Исправлено сравнение

--- src/components/MyMixinValidate.vue
+++ src/components/MyMixinValidate.vue
@@ -9,24 +9,22 @@ export default {
   methods: {
     // Проверка на условия показа сообщения-предупреждения
     isShowMessage() {
-      let isShowMessage = false
-
       if (this.storePeople.people.length == 0) {
-        isShowMessage = true
         this.warningText = 'Прежде чем начать, пожалуйста добавьте людей.'
+        return true
       } else if (this.storePeople.people.length == 1) {
-        isShowMessage = true
         this.warningText = 'Проверьте, что добавлено больше одного человека.'
+        return true
       } else {
+        let isShowMessage = false
         this.storePeople.people.forEach((person) => {
           if (person.name === '') {
             isShowMessage = true
             this.warningText = 'Проверьте, что всем добавленым людям записаны имена.'
           }
         })
+        return isShowMessage
       }
-
-      return isShowMessage
     }
   }
 }
